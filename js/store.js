d2cf93ca7819219e35b712714b5e927c8c853ccb
Ben Shealy <bentshermann@gmail.com>
2021-10-17 13:04:22 -0400

    Cleaned up script syntax

--- js/store.js
+++ js/store.js
@@ -1,14 +1,10 @@
-(function (exports) {
+'use strict'
 
-	'use strict';
-
-	exports.gameStorage = {
-		fetch: function (STORAGE_KEY) {
-			return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
-		},
-		save: function (STORAGE_KEY, data) {
-			localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
-		}
-	};
-
-})(window);
+window.gameStorage = {
+	fetch: function (STORAGE_KEY) {
+		return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+	},
+	save: function (STORAGE_KEY, data) {
+		localStorage.setItem(STORAGE_KEY, JSON.stringify(data))
+	}
+}
